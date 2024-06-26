22f2f0a5a88722f292d31ba9a045750757e47ea7
Varthag <vasilacheanton91@gmail.com>
2024-06-26 13:11:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Frontend Mentor - Password generator app solution
+
+This is a solution to the [Password generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/password-generator-app-Mr8CLycqjh). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Generate a password based on the selected inclusion options
+- Copy the generated password to the computer's clipboard
+- See a strength rating for their generated password
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![screenshot](./screenshot.png)
+
+### Links
+
+- [Solution URL](https://github.com/Antonvasilache/password-generator-app)
+- [Live Site URL](https://password-generator-app-av.netlify.app/)
+
+## My process
+
+1. Building the page and adding the styles.
+2. Adding responsiveness.
+3. Creating the password generator algorithm.
+4. Collecting the inputs from the form.
+5. Adding the copy to clipboard functionality.
+6. Creating the password strength check algorithm and formatting the styles accordingly.
+7. Refactoring some of the functions.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- JavaScript
+
+### What I learned
+
+- Working with custom input elements
+- Working with form data in vanilla JavaScript
+- Creating random character generation using ASCII codes
+
+### Useful resources
+
+- [CSS Custom Range Slider](https://www.sitepoint.com/css-custom-range-slider/) - This article helped me to create my custom range slider and understand how to work around the browser defaults.
+
+## Author
+
+- Website - [Antonvasilache.com](https://www.antonvasilache.com)
+- Frontend Mentor - [@Antonvasilache](https://www.frontendmentor.io/profile/Antonvasilache)
---
+++ starter-code/script.js
@@ -0,0 +1,124 @@
+//Password generator based on the given options selected from the form
+function generatePassword(options) {
+  const lowerCaseRange = { min: 97, max: 122 };
+  const upperCaseRange = { min: 65, max: 90 };
+  const numbersRange = { min: 48, max: 57 };
+  const symbolsRange = { min: 33, max: 47 };
+
+  if (options.length <= 0) return;
+
+  const characterSets = [];
+
+  if (options.lowercase) characterSets.push(lowerCaseRange);
+  if (options.uppercase) characterSets.push(upperCaseRange);
+  if (options.numbers) characterSets.push(numbersRange);
+  if (options.symbols) characterSets.push(symbolsRange);
+
+  if (characterSets.length === 0) return "Select characters";
+
+  let password = "";
+  while (password.length === 0) {
+    password = "";
+    for (let i = 0; i < options.length; i++) {
+      const chosenSet =
+        characterSets[Math.floor(Math.random() * characterSets.length)];
+      const randomCharCode =
+        Math.floor(Math.random() * (chosenSet.max - chosenSet.min + 1)) +
+        chosenSet.min;
+      password += String.fromCharCode(randomCharCode);
+    }
+  }
+
+  return password;
+}
+
+//Checking password strength based four sets of criteria
+function checkPasswordStrength(formData) {
+  const { length, uppercase, lowercase, numbers, symbols } = formData;
+
+  const characterSets = [];
+  if (uppercase) characterSets.push(uppercase);
+  if (lowercase) characterSets.push(lowercase);
+  if (numbers) characterSets.push(numbers);
+  if (symbols) characterSets.push(symbols);
+
+  if (characterSets.length === 0) return "Select characters";
+
+  rectangles.forEach((rectangle) =>
+    rectangle.classList.remove("very-weak", "weak", "medium", "strong")
+  );
+
+  console.log(length);
+
+  if (length <= 4) {
+    rectangles[0].classList.add("very-weak");
+    strength.textContent = "Very weak";
+  }
+
+  if (characterSets.length <= 2 && length > 3 && length <= 20) {
+    rectangles[0].classList.add("weak");
+    rectangles[1].classList.add("weak");
+    strength.textContent = "Weak";
+  }
+
+  if (characterSets.length === 3 && length >= 8 && length <= 12) {
+    rectangles[0].classList.add("medium");
+    rectangles[1].classList.add("medium");
+    rectangles[2].classList.add("medium");
+    strength.textContent = "Medium";
+  }
+
+  if (characterSets.length >= 3 && length >= 12) {
+    rectangles[0].classList.add("strong");
+    rectangles[1].classList.add("strong");
+    rectangles[2].classList.add("strong");
+    rectangles[3].classList.add("strong");
+    strength.textContent = "Strong";
+  }
+}
+
+//Displaying the selected character length
+const range = document.getElementById("range");
+const result = document.getElementById("result");
+const rangeNumber = document.querySelector(".range-number");
+
+range.addEventListener("input", () => {
+  rangeNumber.textContent = range.value;
+});
+
+//Generating the password & displaying password strength
+const form = document.querySelector(".password-options");
+const rectangles = document.querySelectorAll(".rectangle-svg");
+const strength = document.querySelector(".strength-state");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = {
+    length: document.getElementById("range").value,
+    uppercase: document.getElementById("uppercase").checked,
+    lowercase: document.getElementById("lowercase").checked,
+    numbers: document.getElementById("numbers").checked,
+    symbols: document.getElementById("symbols").checked,
+  };
+
+  result.value = generatePassword(formData);
+
+  checkPasswordStrength(formData);
+});
+
+//Copy to clipboard
+const copyBtn = document.querySelector(".copy-btn");
+
+copyBtn.addEventListener("click", () => {
+  const textToCopy = result.value;
+
+  if (
+    navigator.clipboard &&
+    navigator.clipboard.writeText &&
+    textToCopy.length > 0
+  ) {
+    navigator.clipboard
+      .writeText(textToCopy)
+      .catch((error) => console.error("Failed to copy text", error));
+  }
+});
